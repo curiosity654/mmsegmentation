0ff4cac650f1464b7986c20baadbfde9ada49748
curiosity654 <curiosity654@outlook.com>
2021-08-20 13:39:16 +0000

    add deeplabv3 config

---
+++ configs/icip/deeplabv3plus_r101_icip_rgb_all.py
@@ -0,0 +1,63 @@
+_base_ = [
+    '../_base_/datasets/icip_rgb_all.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+
+# model settings
+norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained='open-mmlab://resnet101_v1c',
+    backbone=dict(
+        type='ResNetV1c',
+        in_channels=3,
+        depth=101,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        dilations=(1, 1, 2, 4),
+        strides=(1, 2, 1, 1),
+        norm_cfg=norm_cfg,
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True),
+    decode_head=dict(
+        type='DepthwiseSeparableASPPHead',
+        in_channels=2048,
+        in_index=3,
+        channels=512,
+        dilations=(1, 12, 24, 36),
+        c1_in_channels=256,
+        c1_channels=48,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=1024,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
+
+log_config = dict( 
+    interval=99, 
+    hooks=[ 
+        dict(type='TextLoggerHook'), 
+        dict(type='WandbLoggerHook', init_kwargs=dict(project='ICIP', entity='matlab')), 
+    ])
+
+evaluation = dict(interval=500, metric='mIoU')
+runner = dict(type='IterBasedRunner', max_iters=10000)
+checkpoint_config = dict(by_epoch=False, interval=1000)
